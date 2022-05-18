7ada41f54bc5dc30cfcc8e54533327eff2c724e1
Sungjae Jung <sungjae.jung1031@gmail.com>
2022-05-18 23:56:35 +0900

    add udp socket project

---
+++ socket/udp/client/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/sungjaejung1031/dev/cpp/socket/udp/client
+# Build directory: /Users/sungjaejung1031/dev/cpp/socket/udp/client/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
