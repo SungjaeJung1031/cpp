cab7bf9b0f0308238db96ce8165effa67cb9f424
Sungjae Jung <sungjae.jung1031@gmail.com>
2022-05-20 01:39:07 +0900

    remove cmake build files

--- socket/udp/client/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sungjaejung1031/dev/cpp/socket/udp/client
-# Build directory: /Users/sungjaejung1031/dev/cpp/socket/udp/client/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
